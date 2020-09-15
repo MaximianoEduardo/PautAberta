714a19dc8486b6f2f3f620b2be3d15496b9b17be
MaximianoEduardo <edu.maxi.96@gmail.com>
2020-09-15 18:00:52 -0300

    🚧 Component Home 🚧

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,12 +1,38 @@
 import React from 'react'
+import Card from '../Card'
+import Header from '../Header'
+import { Container, Process, Title, CardWrapper} from './style'
 
 const Home = () => {
 
     return (
 
-        <h1>
-            Home
-        </h1>
+        <Container>
+
+            <Header />
+
+            <Process>
+
+                <Title>
+                    Total de Processos
+                </Title>
+
+                <CardWrapper>
+
+                    <Card  title='Em andamento' description={60} state='working' />
+
+                    <Card  title='Resolvidos' description={16} state='done' />
+
+                    <Card  title='Abertos' description={43} state='default' />
+
+                    <Card  title='Aguardando' description={64} state='stopped' />
+
+                </CardWrapper>
+
+            </Process>
+
+        </Container>
+        
 
     )
 
--- src/components/Home/style.ts
+++ src/components/Home/style.ts
@@ -1,5 +1,42 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const Container = styled.div`
-  
-`;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: auto;
+    min-width: max(calc(100vw - 255px),255px);
+    background: var(--bgColor);
+    padding: 26px 30px;
+`
+
+export const Process = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: auto;
+    background: var(--white);
+    border: 1px solid var(--grayBorder);
+    border-radius: 8px;
+    padding: 24px 27px;
+`
+export const Title = styled.h2`
+    font-size: 1.1875rem;
+    font-family: 'Mulish';
+    font-weight: bold;
+    color: var(--primary);
+    letter-spacing: 0.4px;
+    padding-bottom: min(calc(10vw + 17px), 17px);
+`
+
+export const CardWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    > div{
+        margin-right: 52px;
+        &:last-child{
+            margin-right: unset;
+        }
+    }
+`
